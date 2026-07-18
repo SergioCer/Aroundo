9922bdddc741ff4aa5505dcc14be83899272051e
SergioCer <125737693+SergioCer@users.noreply.github.com>
2026-07-18 11:24:55 +0200

    Add GPS decision tracking function

--- app-events.js
+++ app-events.js
@@ -1,4 +1,5 @@
 import { supabase } from './supabase.js';
+
 export async function trackEvent(action) {
     try {
         await supabase
@@ -13,6 +14,85 @@ export async function trackEvent(action) {
     } catch (error) {
         console.error(
             "Track event error:",
+            error.message
+        );
+    }
+}
+
+export async function trackGPSDecision(action){
+    const device_id =
+    getAnonymousId();
+    const limit =
+    new Date();
+    limit.setHours(
+        limit.getHours() - 24
+    );
+    try {
+        const { data, error } =
+        await supabase
+        .from("statistics")
+        .select("id")
+        .eq(
+            "device_id",
+            device_id
+        )
+        .in(
+            "action",
+            [
+                "GPS_GRANTED",
+                "GPS_PERMISSION_DENIED"
+            ]
+        )
+        .gte(
+            "created_at",
+            limit.toISOString()
+        )
+        .order(
+            "created_at",
+            {
+                ascending:false
+            }
+        )
+        .limit(1);
+        if(error){
+            console.error(
+                "GPS check error:",
+                error.message
+            );
+            return;
+        }
+        if(data && data.length){
+            await supabase
+            .from("statistics")
+            .update({
+                action: action,
+                created_at:
+                new Date()
+                .toISOString()
+            })
+            .eq(
+                "id",
+                data[0].id
+            );
+            return;
+        }
+        await supabase
+        .from("statistics")
+        .insert({
+            action: action,
+            app_version:
+            window.APP_VERSION ?? "unknown",
+            platform:
+            getPlatform(),
+            device_id:
+            device_id,
+            app_mode:
+            getAppMode()
+
+        });
+    } catch(error){
+        console.error(
+            "GPS tracking error:",
             error.message
         );
     }
