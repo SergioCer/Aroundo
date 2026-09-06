eaa0becd73d12985717bb06930df05fceae695b7
SergioCer <125737693+SergioCer@users.noreply.github.com>
2026-09-06 20:28:28 +0200

    Update onboarding.js

--- onboarding/onboarding.js
+++ onboarding/onboarding.js
@@ -158,9 +158,12 @@ en: {
       const margin = 16;
       let left = x - bubble.offsetWidth / 2;
       let top = y + 40; // Spostamento in basso rispetto all'elemento selezionato
-        if (window.innerWidth <= 520) {left = (window.innerWidth - bubble.offsetWidth) / 2 - 20 ;} // il -20 compensa il btn next che sborda
-      const maxLeft = window.innerWidth - bubble.offsetWidth - margin;
-      left = Math.max(margin, Math.min(left, maxLeft));
+if (window.innerWidth <= 520) {
+  left = (window.innerWidth - bubble.offsetWidth) / 2 - 10;
+} else {
+  const maxLeft = window.innerWidth - bubble.offsetWidth - margin;
+  left = Math.max(margin, Math.min(left, maxLeft));
+}      
       const maxTop = window.innerHeight - bubble.offsetHeight - margin;
       top = Math.min(top, maxTop);
       bubble.style.left = `${left}px`;
