5d8b6d754bb6230d9cff71b0f93c5e4e2f029da2
SergioCer <125737693+SergioCer@users.noreply.github.com>
2026-09-07 10:17:50 +0200

    Adjust deltaY calculation for popup positioning

--- onboarding/onboarding.js
+++ onboarding/onboarding.js
@@ -285,7 +285,7 @@ const morePopup = document.querySelector('.leaflet-popup-content-wrapper');
 const moreHeight = morePopup.offsetHeight;
 const screenHeight = window.innerHeight;
 const centerPoint =  mapInstance.latLngToContainerPoint(markerData.marker.getLatLng());
-const deltaY = moreHeight + 100 - screenHeight / 2;
+const deltaY = moreHeight + 70 - screenHeight / 2;
 centerPoint.y += deltaY;
 const moveMore = mapInstance.containerPointToLatLng(centerPoint);
     
